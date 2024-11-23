f0d6669ff68ea69b18faa6172dfa2a183cd38ed8
Mahdi Amirabdollahi <m.amirabdollahi@DOTIN.IR>
2024-11-23 10:22:12 +0330

    feat: MongoTransactionConfig

--- adapters/persistence/src/main/java/ir/dotin/loan/morabehe/adapters/persistence/config/MongoTransactionConfig.java
+++ adapters/persistence/src/main/java/ir/dotin/loan/morabehe/adapters/persistence/config/MongoTransactionConfig.java
@@ -1,12 +1,20 @@
 package ir.dotin.loan.morabehe.adapters.persistence.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "ir.dotin.loan.morabehe.adapters.persistence.repository")
 @EntityScan(basePackages = "ir.dotin.loan")
 public class MongoTransactionConfig {
+
+    @Bean
+    public MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
+    }
 
 }
