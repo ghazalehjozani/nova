8f36b331b034b3003c37192fa7269ba53a5443cf
Mahdi Amirabdollahi <m.amirabdollahi@DOTIN.IR>
2024-11-24 16:00:19 +0330

    refactor: Improve Documents

--- adapters/persistence/src/main/java/ir/dotin/loan/morabehe/adapters/persistence/document/MorabeheLoanRuleDocument.java
+++ adapters/persistence/src/main/java/ir/dotin/loan/morabehe/adapters/persistence/document/MorabeheLoanRuleDocument.java
@@ -1,15 +1,12 @@
 package ir.dotin.loan.morabehe.adapters.persistence.document;
 
+import ir.dotin.loan.baseloan.adapters.persistence.document.AuditableDocument;
 import ir.dotin.loan.baseloan.adapters.persistence.document.loanrule.BaseLoanRuleDocument;
 import java.util.UUID;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("morabehe_loan_rule")
-public class MorabeheLoanRuleDocument {
-
-    @Id
-    private UUID id;
+public class MorabeheLoanRuleDocument extends AuditableDocument {
 
     private BaseLoanRuleDocument loanRule;
 
@@ -17,16 +14,8 @@ public class MorabeheLoanRuleDocument {
     }
 
     public MorabeheLoanRuleDocument(UUID id, BaseLoanRuleDocument loanRule) {
-        this.id = id;
+        super(id);
         this.loanRule = loanRule;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
     }
 
     public BaseLoanRuleDocument getLoanRule() {
