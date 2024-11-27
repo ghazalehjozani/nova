76963231f4980147845eff11dbc27213d7232d2c
Mahdi Amirabdollahi <m.amirabdollahi@DOTIN.IR>
2024-11-27 13:48:14 +0330

    feat: Config server

--- container/src/main/java/ir/dotin/loan/morabehe/container/MorabeheLoanApplication.java
+++ container/src/main/java/ir/dotin/loan/morabehe/container/MorabeheLoanApplication.java
@@ -3,10 +3,8 @@ package ir.dotin.loan.morabehe.container;
 import ir.dotin.loan.baseloan.container.config.ApplicationComponentConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 @Import(ApplicationComponentConfig.class)
 public class MorabeheLoanApplication {
