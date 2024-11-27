a67534585eec1a8c813429ababe841c45526b5d3
f.sotoodeh <f.sotoodeh@dotin.ir>
2024-11-27 15:35:44 +0330

    feat: Morabehe Loan Type Repository

--- core/application/ports/src/main/java/ir/dotin/loan/morabehe/core/application/ports/secondary/MorabeheLoanTypePersistencePort.java
+++ core/application/ports/src/main/java/ir/dotin/loan/morabehe/core/application/ports/secondary/MorabeheLoanTypePersistencePort.java
@@ -1,10 +1,7 @@
 package ir.dotin.loan.morabehe.core.application.ports.secondary;
 
-import ir.dotin.loan.baseloan.domain.config.valueobject.LoanRuleCode;
 import ir.dotin.loan.baseloan.domain.loanapplication.valueobject.LoanTypeCode;
-import ir.dotin.loan.morabehe.core.domain.config.entity.loanrule.MorabeheLoanRule;
 import ir.dotin.loan.morabehe.core.domain.config.entity.loantype.MorabeheLoanType;
-import ir.dotin.loan.morabehe.core.domain.config.valueobject.MorabeheLoanRuleId;
 import ir.dotin.loan.morabehe.core.domain.config.valueobject.MorabeheLoanTypeId;
 
 import java.util.Optional;
