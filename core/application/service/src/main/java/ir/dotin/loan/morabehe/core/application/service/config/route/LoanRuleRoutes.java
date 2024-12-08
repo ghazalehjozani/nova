9eb0830f78afda580c651b4ef30491884763bf55
Fatemeh Sotoodeh <F.Sotoodeh@DOTIN.IR>
2024-12-08 11:22:47 +0330

    feat: Create LoanRule Saga Configurator

---
+++ core/application/service/src/main/java/ir/dotin/loan/morabehe/core/application/service/config/route/LoanRuleRoutes.java
@@ -0,0 +1,34 @@
+package ir.dotin.loan.morabehe.core.application.service.config.route;
+
+import ir.dotin.loan.baseloan.application.service.config.route.BaseRoutes;
+
+public final class LoanRuleRoutes extends BaseRoutes {
+
+    public static final String CREATE_LOAN_RULE_URI = "direct:createLoanRule";
+    public static final String COMPENSATE_CREATE_LOAN_RULE_URI = "direct:compensateCreateLoanRule";
+
+    public static final class SagaRoutes {
+
+        public static final String CREATE_LOAN_RULE_SAGA = "CreateLoanRuleSaga";
+        public static final String CREATE_LOAN_RULE_SAGA_COMPENSATION = CREATE_LOAN_RULE_SAGA + BaseRoutes.SagaRoutes.COMPENSATION;
+
+        private SagaRoutes() {
+        }
+
+    }
+
+    public static final class ApiEndpoints {
+        public static final String BASE_PATH = BaseRoutes.ApiEndpoints.BASE_PATH + "/loan-rule";
+
+        public static final String CREATE = "/create";
+        public static final String COMPENSATE_CREATE = BaseRoutes.ApiEndpoints.COMPENSATE + CREATE;
+
+        private ApiEndpoints() {
+        }
+
+    }
+
+    public LoanRuleRoutes() {
+        super();
+    }
+}
