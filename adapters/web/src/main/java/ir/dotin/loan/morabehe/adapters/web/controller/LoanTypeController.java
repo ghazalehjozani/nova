0091169ae715551b070f2f17f43d8403083a99fb
Fatemeh Sotoodeh <F.Sotoodeh@DOTIN.IR>
2024-12-08 13:07:32 +0330

    refactor: remove LoanRuleRoute

---
+++ adapters/web/src/main/java/ir/dotin/loan/morabehe/adapters/web/controller/LoanTypeController.java
@@ -0,0 +1,42 @@
+package ir.dotin.loan.morabehe.adapters.web.controller;
+
+import ir.dotin.loan.morabehe.core.application.service.command.MorabeheCreateLoanTypeCommand;
+import ir.dotin.loan.morabehe.core.application.service.config.route.LoanTypeRoutes;
+import ir.dotin.loan.morabehe.core.application.service.response.LoanTypeResponse;
+import org.apache.camel.ProducerTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(LoanTypeRoutes.ApiEndpoints.BASE_PATH)
+public class LoanTypeController {
+
+    private final ProducerTemplate producerTemplate;
+
+    public LoanTypeController(ProducerTemplate producerTemplate) {
+        this.producerTemplate = producerTemplate;
+    }
+
+
+    @PostMapping(LoanTypeRoutes.ApiEndpoints.CREATE)
+    public ResponseEntity<LoanTypeResponse> createLoanType(@RequestBody MorabeheCreateLoanTypeCommand command){
+        LoanTypeResponse response = producerTemplate.requestBody(
+                LoanTypeRoutes.CREATE_LOAN_TYPE_URI, command, LoanTypeResponse.class);
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * Compensation Endpoints
+     */
+    @PostMapping(LoanTypeRoutes.ApiEndpoints.COMPENSATE_CREATE)
+    public ResponseEntity<LoanTypeResponse> compensateCreateLoanType(
+            @RequestBody MorabeheCreateLoanTypeCommand command) {
+        LoanTypeResponse response = producerTemplate.requestBody(
+                LoanTypeRoutes.COMPENSATE_CREATE_LOAN_TYPE_URI, command, LoanTypeResponse.class);
+        return ResponseEntity.ok(response);
+    }
+
+}
