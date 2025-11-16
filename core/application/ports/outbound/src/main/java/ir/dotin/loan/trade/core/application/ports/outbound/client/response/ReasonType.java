5e2c1f2c1bdc3046d44fb0465f5cc8c435ca87c4
m.zahedipour <m.zahedipour@dotin.ir>
2025-11-16 08:50:31 +0330

    feat(CBS-256567:LN-58183:LN-58657): implementation of loading reason type
    -implement load reason type for create service feign client
    -implement load reason type for revoke service feign client

--- core/application/ports/outbound/src/main/java/ir/dotin/loan/trade/core/application/ports/outbound/client/response/ReasonType.java
+++ core/application/ports/outbound/src/main/java/ir/dotin/loan/trade/core/application/ports/outbound/client/response/ReasonType.java
@@ -1,9 +1,40 @@
 package ir.dotin.loan.trade.core.application.ports.outbound.client.response;
 
+import ir.dotin.platform.commons.core.Notification;
+import ir.dotin.platform.commons.core.Result;
+import ir.dotin.platform.commons.domain.vo.ValueObject;
+
+import static java.util.Objects.requireNonNull;
+
 public record ReasonType(
         String code,
         String centralBankCode,
         String description,
         String reasonType,
         boolean shouldHasSerial,
-        boolean exemptionOfInquiryNumber) {}
+        boolean exemptionOfInquiryNumber)
+        implements ValueObject<ReasonType> {
+
+    public ReasonType {
+        requireNonNull(code, "ReasonType code cannot be null in constructor");
+        requireNonNull(description, "ReasonType description cannot be null in constructor");
+    }
+
+    public static Result<ReasonType> of(
+            String code,
+            String centralBankCode,
+            String description,
+            String reasonType,
+            boolean shouldHasSerial,
+            boolean exemptionOfInquiryNumber) {
+
+        ReasonType type = new ReasonType(
+                code, centralBankCode, description, reasonType, shouldHasSerial, exemptionOfInquiryNumber);
+
+        Notification internalValidation = type.validate();
+        if (internalValidation.hasErrors()) {
+            return Result.failure(internalValidation);
+        }
+        return Result.success(type);
+    }
+}
