188be97c1c499ad900d8f913e364dfeec240c54d
Fatemeh Sotoodeh <F.Sotoodeh@DOTIN.IR>
2025-12-06 15:38:24 +0330

    refactor(CBS-256567:LN-58612:LN-58614): fix add collateral to loan facility

--- core/application/service/src/main/java/ir/dotin/loan/trade/core/application/service/approvefacility/strategy/AutoApprovalStrategy.java
+++ core/application/service/src/main/java/ir/dotin/loan/trade/core/application/service/approvefacility/strategy/AutoApprovalStrategy.java
@@ -1,16 +1,14 @@
 package ir.dotin.loan.trade.core.application.service.approvefacility.strategy;
 
-import org.springframework.stereotype.Component;
-
-import ir.dotin.platform.commons.core.Notification;
-import ir.dotin.platform.commons.core.Result;
 import ir.dotin.loan.trade.core.application.ports.inbound.command.ApproveFacilityCommand;
 import ir.dotin.loan.trade.core.application.service.approvefacility.i18n.ApproveFacilityErrorCodes;
 import ir.dotin.loan.trade.core.domain.loanarrangement.entity.TradeLoanArrangement;
 import ir.dotin.loan.trade.core.domain.loanfacility.entity.TradeLoanFacility;
 import ir.dotin.loan.trade.core.domain.loanfacility.service.TradeLoanFacilityService;
-
+import ir.dotin.platform.commons.core.Notification;
+import ir.dotin.platform.commons.core.Result;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import static ir.dotin.loan.baseloan.core.domain.loanfacility.enums.ApplicantChannel.DIGITAL_BANK;
 
@@ -30,11 +28,11 @@ public class AutoApprovalStrategy implements ApprovalStrategy {
         }
         boolean isAutoApproval = facility.getLoanApplication().getApplicantChannel() == DIGITAL_BANK;
 
-        //        if (!isAutoApproval) {
-        //            return Result.failure(Notification.ofError(
-        //                    ApproveFacilityErrorCodes.AUTO_APPROVAL_NOT_ENABLED,
-        //                    facility.getLoanApplication().getApplicantChannel().name()));
-        //        }
+        if (!isAutoApproval) {
+            return Result.failure(Notification.ofError(
+                    ApproveFacilityErrorCodes.AUTO_APPROVAL_NOT_ENABLED,
+                    facility.getLoanApplication().getApplicantChannel().name()));
+        }
 
         return Result.success();
     }
